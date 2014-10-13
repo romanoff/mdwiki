3f88db6ce714dc135ff244de0bf59bd5c9a72745
Andriy Romanov <judo.ras@gmail.com>
2014-10-13 16:54:21 -0700

    Added documents index test (failing)

--- wiki/wiki_test.go
+++ wiki/wiki_test.go
@@ -1,7 +1,9 @@
 package wiki
 
 import (
+	"appengine"
 	"appengine/aetest"
+	"appengine/datastore"
 	"encoding/json"
 	"net/http/httptest"
 	"testing"
@@ -25,6 +27,38 @@ func TestDocumentCreate(t *testing.T) {
 		t.Fatalf("Failed to unmarshal document: %v", err)
 	}
 	if document.Key == 0 {
-		t.Errorf("Returned document doesn't have key set")
+		t.Error("Returned document doesn't have key set")
+	}
+}
+
+func TestDocumentsIndex(t *testing.T) {
+	inst, err := aetest.NewInstance(nil)
+	if err != nil {
+		t.Fatalf("Failed to create instance: %v", err)
+	}
+	defer inst.Close()
+	req, err := inst.NewRequest("GET", "/documents", nil)
+	if err != nil {
+		t.Fatalf("Failed to create req: %v", err)
+	}
+	w := httptest.NewRecorder()
+	DocumentsIndex(w, req)
+
+	c := appengine.NewContext(req)
+	var document Document
+	document.Name = "New document"
+	ds := datastore.NewIncompleteKey(c, "Document", nil)
+	_, err = datastore.Put(c, ds, &document)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	var documents []*Document
+	err = json.Unmarshal(w.Body.Bytes(), &documents)
+	if err != nil {
+		t.Fatalf("Failed to unmarshal documents: %v", err)
+	}
+	if len(documents) != 1 {
+		t.Errorf("Expected to get 1 document, but got %v", len(documents))
 	}
 }
